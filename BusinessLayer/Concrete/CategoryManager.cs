b395d58409913753b6b2df3603ef93408fe128cb
LAPTOP-2N07PF7M\yusuf <ybozkurt@mebasoft.com>
2022-05-10 12:22:39 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.EntityFramework;
+using DataAccesLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategroyRepository efCategroyRepository;
+
+        public CategoryManager()
+        {
+            efCategroyRepository = new EfCategroyRepository();
+        }
+        
+        public void categoryAdd(Category category)
+        {
+            efCategroyRepository.Insert(category);
+        }
+
+        public void categoryDelete(Category category)
+        {
+            efCategroyRepository.Delete(category);
+        }
+
+        public void categoryUpdate(Category category)
+        {
+            efCategroyRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategroyRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCategroyRepository.GetListAll();
+        }
+    }
+}
